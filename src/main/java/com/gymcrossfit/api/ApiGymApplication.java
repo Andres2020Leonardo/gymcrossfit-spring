8a660158b0bb1a592fb08ba018947bc8e5a55554
Andres2020Leonardo <andruuescobar@gmail.com>
2022-06-13 12:35:58 -0500

    primer commit

---
+++ src/main/java/com/gymcrossfit/api/ApiGymApplication.java
@@ -0,0 +1,13 @@
+package com.gymcrossfit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGymApplication.class, args);
+	}
+
+}
